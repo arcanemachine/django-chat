5469ee872ba0f42d62534c81228730a36af04e10
Nicholas Moen <arcanemachine@tutanota.com>
2021-01-08 19:02:16 -0700

    Create hello_world view in Django Rest Framework

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
+# from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from . import serializers
 from .permissions import HasMessagePermissionsOrReadOnly
@@ -8,17 +10,41 @@ from chat.models import Conversation, Message
 
 UserModel = get_user_model()
 
-#class MessageCreate(generics.APIView):
-#    permission_classes = [HasMessagePermissionsOrReadOnly]
-#    serializer_class = serializers.MessageSerializer
+
+# class MessageCreate(generics.APIView):
+#     permission_classes = [HasMessagePermissionsOrReadOnly]
+#     serializer_class = serializers.MessageSerializer
 #
-#    def get_queryset(self):
-#        return Conversation.objects.filter(pk=self.kwargs['conversation_pk'])
+#     def get_queryset(self):
+#         return Conversation.objects.filter(pk=self.kwargs['conversation_pk'])
+
+
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'POST':
+        return Response({
+            'message': "You sent a POST request!",
+            'data': request.data})
+    return Response({'message': "Hello world!"})
+
 
 class MessageList(generics.ListCreateAPIView):
     permission_classes = [HasMessagePermissionsOrReadOnly]
     serializer_class = serializers.MessageSerializer
+    lookup_url_kwarg = 'conversation_pk'
+
+    def get_queryset(self):
+        return Conversation.objects.get(
+            pk=self.kwargs['conversation_pk']).message_set.all()
+
+
+class MessageCountList(generics.ListCreateAPIView):
+    permission_classes = [HasMessagePermissionsOrReadOnly]
+    serializer_class = serializers.MessageSerializer
     lookup_url_kwarg = 'message_pk'
+
+    def get_queryset(self):
+        return Conversation.objects.filter(pk=self.kwargs['conversation_pk'])
 
 
 class MessageDetail(generics.RetrieveUpdateDestroyAPIView):
