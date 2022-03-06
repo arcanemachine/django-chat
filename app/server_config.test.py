d51331b5ad551702c957cd12c1c06b78676c1c9c
Nicholas Moen <arcanemachine@tutanota.com>
2022-03-05 22:20:09 -0700

    Bug fixes and performace improvements

--- app/server_config.test.py
+++ app/server_config.test.py
@@ -5,7 +5,7 @@ SERVER_NAME = 'test'
 DEBUG = False
 
 BASE_DIR = str(Path(__file__).resolve().parent)
-ALLOWED_HOSTS = ['django-chat.test.moendigitalservices.com']
+ALLOWED_HOSTS = ['*']
 
 STATICFILES_DIRS = [os_path_join(BASE_DIR, 'static')]
 STATIC_ROOT = os_path_join(BASE_DIR, 'staticfiles')
