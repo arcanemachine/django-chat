1e4c312baaf01ea78dcb18a17a3ddf3caf861a3c
Nicholas Moen <arcanemachine@tutanota.com>
2022-03-06 00:32:22 -0700

    Add ALLOWED_HOSTS for prod

--- app/server_config.prod.py
+++ app/server_config.prod.py
@@ -5,7 +5,7 @@ SERVER_NAME = 'prod'
 DEBUG = True
 
 BASE_DIR = str(Path(__file__).resolve().parent)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-chat.nicholasmoen.com']
 
 STATICFILES_DIRS = [os_path_join(BASE_DIR, 'static')]
 STATIC_ROOT = os_path_join(BASE_DIR, 'staticfiles')
