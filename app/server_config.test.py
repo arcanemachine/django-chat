376b33530bdff11836c72f4c32c54dc71a759173
Nicholas Moen <arcanemachine@tutanota.com>
2022-03-21 01:38:34 -0600

    Use shorter test domain name

--- app/server_config.test.py
+++ app/server_config.test.py
@@ -5,12 +5,12 @@ SERVER_NAME = 'test'
 DEBUG = False
 
 BASE_DIR = str(Path(__file__).resolve().parent)
-ALLOWED_HOSTS = ['django-chat.nicholasmoen.com.test.moendigitalservices.com']
+ALLOWED_HOSTS = ['django-chat.test.moendigitalservices.com']
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS =\
-    ['https://django-chat.nicholasmoen.com.test.moendigitalservices.com']
+    ['https://django-chat.test.moendigitalservices.com']
 CSRF_TRUSTED_ORIGINS =\
-    ['https://django-chat.nicholasmoen.com.test.moendigitalservices.com']
+    ['https://django-chat.test.moendigitalservices.com']
 
 STATICFILES_DIRS = [os_path_join(BASE_DIR, 'static')]
 STATIC_ROOT = os_path_join(BASE_DIR, 'staticfiles')
